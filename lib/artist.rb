9052cca26dc4e53a51f47ef4f572072e1e0603f2
Dana Palmer <roiai4@hotmail.com>
2018-10-21 00:59:00 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   def initialize (name)
     @name = name
@@ -13,5 +13,6 @@ class Artist
   def add_song(song)
     @songs << song
   end
+
 
 end
