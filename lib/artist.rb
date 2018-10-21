068d74b743fb94f560d1dfacaa1657b8d3861f5b
Dana Palmer <roiai4@hotmail.com>
2018-10-21 00:57:47 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,18 @@
 class Artist
   attr_accessor :name
-  @@songs = []
+  @@all = []
 
   def initialize (name)
     @name = name
+    @songs = []
   end
 
   def self.songs(song_one)
     song = Song.new(song_one)
   end
 
-  def add_song(song_title)
-    song = Song.new(song_title)
-    @@songs << song
+  def add_song(song)
+    @songs << song
   end
 
 end
