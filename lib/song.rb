b2af4cc67fef95e25948403296493bb935e63d39
Dana Palmer <roiai4@hotmail.com>
2018-10-21 03:24:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,4 +5,11 @@ class Song
     @title = title
   end
 
+  def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_name = Song.new(song)
+    new_name.artist = artist
+    new_song.save
+  end
+
 end
