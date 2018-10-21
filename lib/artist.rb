1cbd01beed00c78c85c591d1331165554ad53d2b
Dana Palmer <roiai4@hotmail.com>
2018-10-21 03:57:49 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,11 +25,24 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    @@all.detect do |artist| artist == artist.name  ||=
-      artist = Artist.new(name)
-      artist.name = name
-    end
-end
+    self.find(name) ? self.find(name) : self.create(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+
+  def self.create(name)
+    self.new(name).tap {|artist| artist.save}
+  end
+
+  #def self.find_or_create_by_name(name)
+    #@@all.detect do |artist| artist == artist.name  ||=
+    #  artist = Artist.new(name)
+    #  artist.name = name
+  #  end
+#end
 
 def print_songs
   songs.each do |song|
