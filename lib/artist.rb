6f255292c0bbff8817e75696ae89862277d4d950
Dana Palmer <roiai4@hotmail.com>
2018-10-21 01:16:58 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,7 +25,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    if @@all.select do |artist| artist == artist.name
+    @@all.select do |artist| artist == artist.name
     else
       artist = Artist.new(name)
       artist.name = name
